a1a13d4896d0898d65a0e0f3486596c61ddddade
Patrick Morais <ppternunes@gmail.com>
2021-06-22 12:38:50 -0300

    letmeask - newRoom

---
+++ src/pages/NewRoom.tsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImg from '../assets/images/logo.svg';
+import googleIcon from '../assets/images/google-icon.svg'
+import '../styles/auth.scss'
+import Button from '../components/Button';
+
+
+const Home = () => {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao-vivo</strong>
+        <p>Tire dúvidas do seu público em tempo real!</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Logo da aplicação" />
+          <h2>Criar uma nova sala</h2>
+          <form action="">
+            <input
+              type="text"
+              placeholder="Nome da sala"
+            />
+            <Button type="submit">
+              Criar sala
+            </Button>
+          </form>
+          <p>Quer entrar uma sala existente? <a href="#">Clique aqui</a></p>
+        </div>
+      </main>
+    </div>
+  )
+}
+
+export default Home;
