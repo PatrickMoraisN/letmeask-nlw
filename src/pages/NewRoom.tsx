a31c5e12bd860bb4c1f3630ab157beb8acbf27e1
Patrick Morais <ppternunes@gmail.com>
2021-06-23 11:30:27 -0300

    letmeask - newroom / link

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
-import googleIcon from '../assets/images/google-icon.svg'
 import { Link } from 'react-router-dom';
 import '../styles/auth.scss'
 import Button from '../components/Button';
