2815e43d44a73d654602e272ace692277b4460ac
Patrick Morais <ppternunes@gmail.com>
2021-06-24 20:48:34 -0300

    letmeask - Room / html

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React from "react";
+import imgLogo from "../assets/images/logo.svg";
+import Button from "../components/Button";
 
 const Room = () => {
   return (
-    <h1>
-      sala
-    </h1>
-  )
-}
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={imgLogo} alt="letmeask" />
+          <div>codigo</div>
+        </div>
+      </header>
 
-export default Room
+      <main className="content">
+        <div className="room-title">
+          <h1>sala react</h1>
+          <span>4 perguntas</span>
+        </div>
+
+        <form >
+          <textarea
+            placeholder="Pergunte algo!"
+          />
+
+          <div className="form-footer">
+            <span>Para enviar uma pergunta, <button>Faca seu login</button></span>
+            <Button type="submit">Envia pergunta</Button>
+          </div>
+        </form>
+      </main>
+    </div>
+  );
+};
+
+export default Room;
